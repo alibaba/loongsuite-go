bfee5287b744390746ab695be37a6b55164c3bc4
everfid-ever <166227111+everfid-ever@users.noreply.github.com>
2025-12-13 18:14:37 +0800

    add comments for data type

--- pkg/rules/mqtt/mqtt_data_type.go
+++ pkg/rules/mqtt/mqtt_data_type.go
@@ -16,22 +16,31 @@ package mqtt
 
 import "github.com/mochi-mqtt/server/v2/packets"
 
-// PublishRequest represents an inbound publishing from client to broker
+// PublishRequest represents a request to publish a message from an MQTT client to the broker.
+// It is used in MQTT instrumentation to capture the details of a publish operation initiated by a client.
+// The Packet field contains the MQTT publish packet, ClientID identifies the publishing client,
+// and Remote provides the sender's remote address.
 type PublishRequest struct {
 	Packet   *packets.Packet // The MQTT publish packet
 	ClientID string          // ID of the client publishing the message
 	Remote   string          // sender remote address
 }
 
-// PublishResponse is a placeholder for producer-side result.
+// PublishResponse represents the result of processing a PublishRequest on the producer (client) side.
+// This type is used as a placeholder for future extensions where additional response information
+// may be needed after a publish operation. Currently, it does not contain any fields.
 type PublishResponse struct{}
 
-// DeliverRequest represents a broker -> subscriber delivery.
+// DeliverRequest represents a request to deliver a published message from the broker to a subscriber.
+// It is used in MQTT instrumentation to capture the details of a message delivery operation.
+// The Packet field contains the publish packet being delivered, ClientID identifies the subscriber client,
+// and Remote provides the subscriber's remote address.
 type DeliverRequest struct {
 	Packet   packets.Packet // publish packet being delivered
 	ClientID string         // subscriber client id
 	Remote   string         // subscriber remote address
 }
 
-// DeliverResponse is a placeholder for consumer-side result.
-type DeliverResponse struct{}
+// DeliverResponse represents the result of processing a DeliverRequest on the consumer (subscriber) side.
+// This type is used as a placeholder for future extensions where additional response information
+// may be needed after a delivery operation. Currently, it does not contain any fields.
